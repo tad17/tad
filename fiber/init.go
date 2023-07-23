2a5becac60160387837defd5b8d8432e3fbc89e2
Анатолий Тухтаров <admin@tad17.ru>
2023-07-23 11:22:35 +0500

    v0.0.2
    Добавил инициализацию app

--- fiber/init.go
+++ fiber/init.go
@@ -1,10 +1,21 @@
 package fiber
 
-import(
+import (
 	"fmt"
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/compress"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/favicon"
+	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
-func Init() {
-	fmt.Printf("init fiber\n")
+func NewFiber() *fiber.App {
+	// инициализация fiber
+	app := fiber.New()
+	app.Use(logger.New())
+	app.Use(cors.New())
+	app.Use(favicon.New())
+	app.Use(compress.New())
+	fmt.Printf("fiber init\n")
+	return app
 }
-	
